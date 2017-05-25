aa69e3cbc6a4d83e036369e3d58b055216b4357f
Ioannis Gabrielides <ioannisgabrielides@gmail.com>
2017-05-25 21:42:48 +0100

    [IG] Created ticket object

---
+++ server/db.js
@@ -0,0 +1,3 @@
+/**
+ * Created by yianni on 25/05/17.
+ */
---
+++ server/ticket.js
@@ -0,0 +1,11 @@
+/**
+ * Created by yianni on 25/05/17.
+ */
+
+function Ticket(id, desc, column) {
+    this.id = id;
+    this.desc = desc;
+    this.column = column;
+}
+
+module.exports.Ticket = Ticket;
