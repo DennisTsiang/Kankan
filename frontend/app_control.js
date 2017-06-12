ade997a597d093e68d4ea317d98e06e9b5327d4b
Harry <harryonline3@hotmail.co.uk>
2017-06-12 18:05:02 +0100

    [HM] Can now route back to home from overview

--- frontend/app_control.js
+++ frontend/app_control.js
@@ -28,6 +28,16 @@ app.config(function($routeProvider) {
 
 app.controller('OverviewController', function($scope, $location){
 
+  if (get_kanban_scope().username === undefined) {
+    $location.path('/login');
+  } else {
+    $scope.username = get_kanban_scope().username;
+  }
+
+  $scope.goHome = function () {
+    $location.path('/home');
+  };
+
 });
 
 app.controller('ApplicationCtrl', function($scope, $location) {
